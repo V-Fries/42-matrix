58f8ffcee9e85729661513ca6fd7e60f895c8d21
V-Fries <vincentfries@proton.me>
2024-08-29 19:42:43 +0200

    Matrix::trace() implemented

--- src/matrix/mod.rs
+++ src/matrix/mod.rs
@@ -5,5 +5,6 @@ mod mul;
 mod comparison;
 mod mul_vector;
 mod mul_matrix;
+mod trace;
 
 pub use matrix::Matrix;
---
+++ src/matrix/trace.rs
@@ -0,0 +1,40 @@
+use std::ops::Add;
+
+use super::Matrix;
+
+impl<K, const N: usize> Matrix<K, N, N> 
+    where
+        K: Default + for<'a> Add<&'a K, Output = K>
+{
+    fn trace(&self) -> K {
+        (0..N).fold(K::default(), |acc, i| {
+            acc + &self[i][i]
+        })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_trace() {
+        let u = Matrix::new([
+            [1., 0.],
+            [0., 1.],
+        ]);
+        assert_eq!(u.trace(), 2.);
+        let u = Matrix::new([
+            [2., -5., 0.],
+            [4., 3., 7.],
+            [-2., 3., 4.],
+        ]);
+        assert_eq!(u.trace(), 9.);
+        let u = Matrix::new([
+            [-2., -8., 4.],
+            [1., -23., 4.],
+            [0., 6., 4.],
+        ]);
+        assert_eq!(u.trace(), -21.);
+    }
+}
